61d3ed0911521687caffc6791aea01b72867a84b
jane42070 <jql1377219787@gmail.com>
2020-02-18 22:03:58 +0800

    Added volume control

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 15;        /* gaps between windows */
+static const unsigned int gappx     = 17;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Hack Nerd Font:size=10" };
-static const char dmenufont[]       = "Hack Nerd Font:size=10";
+static const char *fonts[]          = { "Hack Nerd Font:size=12" };
+static const char dmenufont[]       = "Hack Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -67,14 +67,20 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[] = { "st", NULL };
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
+static const char *volumeup[] = { "amixer", "-q", "sset", "Master", "1+", NULL };
+static const char *volumedown[] = { "amixer", "-q", "sset", "Master", "1-", NULL };
+static const char *volumemute[] = { "amixer", "-q", "sset", "Master", "toggle", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_g,      spawn,          {.v = browsercmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	/* modifier                     key			function        argument */
+	{ MODKEY,                       XK_p,		spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_g,		spawn,          {.v = browsercmd } },
+	{ MODKEY|ShiftMask,             XK_Return,	spawn,          {.v = termcmd } },
+	{ MODKEY,						XK_Up,		spawn,          {.v = volumeup } },   //音量增加
+    { MODKEY,						XK_Down,	spawn,          {.v = volumedown } }, //音量减小
+    { MODKEY,						XK_F12,     spawn,          {.v = volumemute } }, //静音
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
