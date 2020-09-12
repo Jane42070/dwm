d1c5abec62d7e7503f4f0f9df1d95d14bd48072d
Jane42070 <jql137719787@gmail.com>
2020-09-12 19:46:14 +0800

    INC BORDERPX, CHANGE COLORSCHEME

--- config.h
+++ config.h
@@ -1,20 +1,31 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 14;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Hack Nerd Font:size=14" };
-static const char dmenufont[]       = "Hack Nerd Font:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-// static const char col_cyan[]        = "#005577";
-static const char col_cyan[]        = "#42A5F5";
+static const char *fonts[]          = { "Hack Nerd Font:size=13" };
+static const char dmenufont[]       = "Hack Nerd Font:size=13";
+
+// status bar color
+// #222222
+static const char col_gray1[]       = "#272727";
+// border color
+// #444444
+static const char col_gray2[]       = "#747474";
+// unselected fonts color
+// #bbbbbb
+static const char col_gray3[]       = "#FF652F";
+// selected fonts color
+// #eeeeee
+static const char col_gray4[]       = "#FFE400";
+// selected window color
+// #005577
+static const char col_cyan[]        = "#14A76C";
+
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -93,7 +104,7 @@ static const char *volumemute[] = { "amixer", "-q", "set", "Master", "toggle", N
 static const char *backlightinc[] = { "xbacklight", "-inc", "10", NULL};
 static const char *backlightdec[] = { "xbacklight", "-dec", "10", NULL};
 static const char *screenshot[]   = {"scrot", NULL};
-static const char *flameshot[]   = {"sh", "~/my-scripts/screenshot.sh", NULL};
+static const char *flameshot[]   = {"sh", "~/.config/my-scripts/screenshot.sh", NULL};
 
 static Key keys[] = {
 	/* modifier                     key		   function        argument */
