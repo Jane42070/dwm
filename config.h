98c4841dca8461b3c04c524b5c7022978a4a2a1a
Jane42070 <jql1377219787@gmail.com>
2020-12-28 16:35:23 +0800

    better padding

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx    = 3;        /* border pixel of windows */
-static const unsigned int gappx       = 14;        /* gaps between windows */
-static const unsigned int snap        = 32;       /* snap pixel */
+static const unsigned int borderpx    = 2;        /* border pixel of windows */
+static const unsigned int gappx       = 5;        /* gaps between windows */
+static const unsigned int snap        = 10;       /* snap pixel */
 static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
 static const Bool viewontag           = True;     /* Switch view on tag switch */
